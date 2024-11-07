d1652600eac3ea3f32590fa7b0350e65c1caef08
AlisonCastroM <alisoncastro7616@gmail.com>
2024-11-07 23:35:00 +0000

    encaminado

---
+++ src/js/views/createContact.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+
+export const CreateContact = () => {
+    const [newContact, setNewContact] = useState({});
+    return (<>
+        <div className="container">
+            <div className="mb-3">
+                <label for="exampleInputEmail1" className="form-label fw-bold">Full name</label>
+                <input onChange={(evento) => setNewContact({...newContact, name:evento.target.value})} value={newContact.name|| ''} type="text" className="form-control" placeholder="Full name" />
+                <div className="form-text">We'll never share your email with anyone else.</div>
+            </div>
+
+            <div className="mb-3">
+                <label for="exampleInputEmail1" className="form-label fw-bold">Email</label>
+                <input onChange={(evento) => setNewContact({...newContact, email:evento.target.value})} value={newContact.email|| ''} type="email" className="form-control" placeholder="Email" />
+                <div className="form-text">We'll never share your email with anyone else.</div>
+            </div>
+
+            <div className="mb-3">
+                <label for="exampleInputEmail1" className="form-label fw-bold">Enter phone</label>
+                <input onChange={(evento) => setNewContact({...newContact, phone:evento.target.value})} value={newContact.phone|| ''} type="phone" className="form-control" placeholder="Enter phone" />
+                <div className="form-text">We'll never share your email with anyone else.</div>
+            </div>
+
+            <div className="mb-3">
+                <label for="exampleInputEmail1" className="form-label fw-bold">Enter address</label>
+                <input onChange={(evento) => setNewContact({...newContact, address:evento.target.value})} value={newContact.address|| ''} type="text" className="form-control" placeholder="Enter address"/>
+                <div className="form-text">We'll never share your email with anyone else.</div>
+            </div>
+
+            <button onClick={ () => {console.log()}} type="submit" className="btn btn-primary">Submit</button>
+        </div>
+    </>
+    );
+}
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,24 @@
 import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
+import { useContext, useEffect, useState } from "react";
+import { Context } from "../store/appContext";
+import { ContactCard } from "../component/card";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+
+
+export const Home = () => {
+
+	const {actions, store} = useContext(Context);
+
+	return (
+		<div className="text-center mt-5">
+			<h1>Hello Rigo!</h1>
+			{
+				store.contacts.map((item, index) =>{
+					return <ContactCard key = {index} name ={item.name} /> 
+				})
+			}
+
+		</div>
+	);
+}
